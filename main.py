7ea78916564b1d20f16558058a8f8a6b15d90e5c
cosmickrish <krishtech@outlook.in>
2024-08-01 18:30:39 +0530

    break long lines, one more line

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ iris = load_iris()
 X, y = iris.data, iris.target
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=31)
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=31)
 
 # Train the logistic regression model
 model = RandomForestClassifier(
